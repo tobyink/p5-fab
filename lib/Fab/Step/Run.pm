f530e654c9b231745e07917783b49bd5d21b6acd
Toby Inkster <mail@tobyinkster.co.uk>
2022-10-08 15:20:37 +0100

    Minor restructuring of Fab::Step::Run to make it easier to test

--- lib/Fab/Step/Run.pm
+++ lib/Fab/Step/Run.pm
@@ -33,43 +33,11 @@ sub execute ( $self, $context ) {
 	);
 	
 	if ( ref($command) eq 'CODE' ) {
-		try {
-			local $Fab::CONTEXT = $context;
-			local $Fab::STEP    = $self;
-			$command->( @args );
-		}
-		catch ( $e ) {
-			'Fab::Exception::StepFailed'->throw(
-				error => $e,
-				step  => $self,
-				original_exception => $e,
-			);
-		};
-		return;
+		return $self->_execute_coderef( $context, $command, \@args );
 	}
-	
-	if ( blessed $command and $command->isa( 'Path::Tiny' ) ) {
-		$command = $command->stringify;
+	else {
+		return $self->_execute_binary( $context, $command, \@args );
 	}
-	
-	my $in  = $context->get_setting( 'stdin'  ) // \*STDIN;
-	my $out = $context->get_setting( 'stdout' ) // \*STDOUT;
-	my $err = $context->get_setting( 'stderr' ) // \*STDERR;
-	my $ok  = IPC::Run::run(
-		@args ? [ $command, @args ] : [ qw( sh -c ), $command ], # XXX: windows
-		'<'  => $in,
-		'>'  => $out,
-		'2>' => $err,
-	);
-	
-	if ( not $ok ) {
-		'Fab::Exception::StepFailed'->throw(
-			error => "process failed",
-			step  => $self,
-		);
-	}
-	
-	return;
 }
 
 sub _process_args ( $self, $context ) {
@@ -84,6 +52,52 @@ sub _process_arg ( $self, $arg, $context ) {
 		return $arg->stringify;
 	}
 	return $arg;
+}
+
+sub _execute_coderef ( $self, $context, $command, $args ) {
+	
+	try {
+		local $Fab::CONTEXT = $context;
+		local $Fab::STEP    = $self;
+		$command->( $args->@* );
+	}
+	catch ( $e ) {
+		'Fab::Exception::StepFailed'->throw(
+			error => $e,
+			step  => $self,
+			original_exception => $e,
+		);
+	};
+	return;
+}
+	
+sub _execute_binary ( $self, $context, $command, $args ) {
+	
+	if ( blessed $command and $command->isa( 'Path::Tiny' ) ) {
+		$command = $command->stringify;
+	}
+	
+	my $in  = $context->get_setting( 'stdin'  ) // \*STDIN;
+	my $out = $context->get_setting( 'stdout' ) // \*STDOUT;
+	my $err = $context->get_setting( 'stderr' ) // \*STDERR;
+	
+	my $ok  = IPC::Run::run(
+		$args->@*
+			? [ $command, $args->@* ]
+			: [ qw( sh -c ), $command ], # XXX: windows
+		'<'  => $in,
+		'>'  => $out,
+		'2>' => $err,
+	);
+	
+	if ( not $ok ) {
+		'Fab::Exception::StepFailed'->throw(
+			error => "process failed",
+			step  => $self,
+		);
+	}
+	
+	return;
 }
 
 1;
