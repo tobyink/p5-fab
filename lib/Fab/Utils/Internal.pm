54688f5f5c1515828a2188772155d7d679a2fc0d
Toby Inkster <mail@tobyinkster.co.uk>
2022-10-10 18:00:06 +0100

    Avoid warnings on Perl 5.36.

--- lib/Fab/Utils/Internal.pm
+++ lib/Fab/Utils/Internal.pm
@@ -8,7 +8,8 @@ use parent 'Exporter::Tiny';
 
 our @EXPORT_OK = qw( rethrow );
 
-sub rethrow ( $exception ) {
+sub rethrow {
+	my ( $exception ) = @_;
 	
 	if ( Scalar::Util::blessed($exception) and $exception->isa( 'Fab::Exception' ) ) {
 		my $next = $exception->can( 'rethrow' );
